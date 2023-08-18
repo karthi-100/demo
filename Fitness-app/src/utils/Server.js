37e048054c4830357e867c41598e246f6b845cb8
karthi-100 <karthivicky42@gmail.com>
2023-08-18 16:36:17 +0530

    commit

--- Fitness-app/src/utils/Server.js
+++ Fitness-app/src/utils/Server.js
@@ -1,5 +1,12 @@
 export const exerciseOptions = {
 	method: 'GET',
+    mode:'cors',
+    headers: {
+        'Access-Control-Allow-Credentials' : true,
+        'Access-Control-Allow-Origin':'*',
+        'Access-Control-Allow-Methods':'GET',
+        'Access-Control-Allow-Headers':'application/json',
+      },
 };
 export const fetchData=async (url,options) =>{
     const response=await fetch(url,options);

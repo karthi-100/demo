be1530d56d63102e35ac1cda0c93ef04f62e6aca
karthi-100 <karthivicky42@gmail.com>
2024-03-01 17:43:27 +0530

    eviromentalvariable

--- server/Server.js
+++ server/Server.js
@@ -1,30 +1,29 @@
-
-var express=require('express')
-var app=express()
-var  cors= require('cors')
-app.use(cors())
-var {MongoClient,ServerApiVersion}=require('mongodb')
-const uri='mongodb+srv://karthivicky:VF5aF0KsGI5pUPAl@workoutapi.giighg8.mongodb.net/?retryWrites=true&w=majority'
+var express = require("express");
+var app = express();
+require("dotenv").config();
+var cors = require("cors");
+app.use(cors());
+var { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = process.env.MONGO_URL;
 const client = new MongoClient(uri, {
-    serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
-    }
-  });
-  
-app.get('/workouts',async(req,res)=>{
-  const db=client.db('Workouts')
-  const data= await db.collection('Exercise').find().toArray()
-  res.json(data)
-})
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
-app.get('/BodyParts',async(req,res)=>{
-  const db=client.db('BodyParts')
-const data=await db.collection('Parts').find().toArray()
-res.json(data)
-console.log(data);
-})
+app.get("/workouts", async (req, res) => {
+  const db = client.db("Workouts");
+  const data = await db.collection("Exercise").find().toArray();
+  res.json(data);
+});
 
-app.listen(3000)
+app.get("/BodyParts", async (req, res) => {
+  const db = client.db("BodyParts");
+  const data = await db.collection("Parts").find().toArray();
+  res.json(data);
+  console.log(data);
+});
 
+app.listen(3000);
